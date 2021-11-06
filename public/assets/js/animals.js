acac1b6583fde67509567842b7b81cd4231c9652
Bailey Poe <baileypoe1222@gmail.com>
2021-11-06 15:43:50 -0500

    added functionality for add zookeeper form

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -1,5 +1,3 @@
-const { response } = require("express");
-
 const $animalForm = document.querySelector('#animals-form');
 const $displayArea = document.querySelector('#display-area');
 
@@ -36,14 +34,14 @@ const getAnimals = (formData = {}) => {
   fetch(queryUrl)
     .then(response => {
       if (!response.ok) {
-        return alert('Error: ' + response.statusText)
+        return alert('Error: ' + response.statusText);
       }
       return response.json();
     })
     .then(animalData => {
       console.log(animalData);
       printResults(animalData);
-    })
+    });
 };
 
 const handleGetAnimalsSubmit = event => {
